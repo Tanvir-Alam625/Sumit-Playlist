0bbebb10c4920f0b79d5c5febc0f1a1858cb35c0
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-08-19 19:22:41 +0600

    googleMap compo deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import GoogleMap1 from './Components/GoogleMap';
 import VideoPlayer from './Components/VideoContainer';
 function App() {
   return (
@@ -8,8 +7,7 @@ function App() {
      <header className='py-8'>
       <h2 className='text-4xl text-center text-white tracking-wide '>Talk With Sumit</h2>
      </header>
-     {/* <VideoPlayer/> */}
-     <GoogleMap1/>
+     <VideoPlayer/>
     </div>
   );
 }
